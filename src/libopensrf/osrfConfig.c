56eac457879931d1ec9e296b0366292fa305a069
miker <miker@9efc2488-bf62-4759-914b-345cdb29e865>
2007-06-28 03:14:06 +0000

    Patch from Scott McKellar:
    At one time. if we had trouble reading or parsing the configuration
    file, we would issue log messages to complain.  However those log
    messages didn't have anywhere to go because, not having loaded the
    configuration file, we didn't know where to write the messages.
    
    The earlier patch wrote the same messages to standard error as well
    as to the logging machinery, so that they would be visible.
    
    These extra writes are no longer necessary,  With recent changes to
    the logging machinery, all log messages will go to standard error
    if no other destination has been defined for them.
    
    Accordingly this patch removes two messages, now redundant, that
    were being written to standard error.
    
    
    
    git-svn-id: svn://svn.open-ils.org/OpenSRF/trunk@980 9efc2488-bf62-4759-914b-345cdb29e865

--- src/libopensrf/osrfConfig.c
+++ src/libopensrf/osrfConfig.c
@@ -45,7 +45,6 @@ osrfConfig* osrfConfigInit(char* configFile, char* configContext) {
 	
 	xmlDocPtr doc = xmlParseFile(configFile);
 	if(!doc) {
-		fprintf( stderr, "osrfConfigInit: Unable to parse XML config file %s\n", configFile);
 		osrfLogWarning( OSRF_LOG_MARK, "Unable to parse XML config file %s", configFile);
 		return NULL;
 	}
@@ -56,7 +55,6 @@ osrfConfig* osrfConfigInit(char* configFile, char* configContext) {
 	xmlFreeDoc(doc);
 
 	if(!json_config ) {
-		fprintf( stderr, "osrfConfigInit: xmlDocToJSON failed for config %s\n", configFile);
 		osrfLogWarning( OSRF_LOG_MARK, "xmlDocToJSON failed for config %s", configFile);
 		return NULL;
 	}	
