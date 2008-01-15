ded6bea9609317fb96398c67319550601bf89618
dbs <dbs@9efc2488-bf62-4759-914b-345cdb29e865>
2008-01-15 04:03:07 +0000

    Switch to setuptools to enable dependency installs as well
    git-svn-id: svn://svn.open-ils.org/OpenSRF/trunk@1212 9efc2488-bf62-4759-914b-345cdb29e865

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
-from distutils.core import setup
-import os, os.path
-
-dir = os.path.dirname(__file__)
+from setuptools import setup
 
 setup(name='OpenSRF',
     version='1.0',
-# requires is not actually implemented in distutils
-#    requires=['memcache', 'pyxmpp', 'simplejson'],
+    install_requires=[
+        'python_memcached>=1.40',
+        'pyxmpp>=1.0.0',
+        'simplejson>=1.7.1'
+    ],
+    dependency_links = [
+        "http://pyxmpp.jajcus.net/downloads/"
+    ],
     description='OpenSRF Python Modules',
     author='Bill Erickson',
-    author_email='open-ils-dev@list.georgialibraries.org',
+    author_email='erickson@esilibrary.com',
+    license="GPL",
     url='http://www.open-ils.org/',
     packages=['osrf'],
-    package_dir={'': dir},
-    scripts=[os.path.join(dir, 'srfsh.py')]
+    package_dir={'': '.'},
+    scripts=['./srfsh.py']
 )
