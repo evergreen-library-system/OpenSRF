a5c6655c405e726cc7940d79a06651aaa5a8f6f1
dbs <dbs@9efc2488-bf62-4759-914b-345cdb29e865>
2008-01-16 17:40:07 +0000

    Add dnspython, libxml, and libxslt1 dependencies on behalf of pyxmpp
    git-svn-id: svn://svn.open-ils.org/OpenSRF/trunk@1221 9efc2488-bf62-4759-914b-345cdb29e865

--- src/python/setup.py
+++ src/python/setup.py
@@ -5,6 +5,9 @@ from setuptools import setup
 setup(name='OpenSRF',
     version='1.0',
     install_requires=[
+        'dnspython', # required by pyxmpp
+        'libxml2', # required by pyxmpp
+        'libxslt1',
         'python_memcached',
         'pyxmpp>=1.0.0',
         'simplejson>=1.7.1'
