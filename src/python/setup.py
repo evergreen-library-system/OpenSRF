e8a4a367fbc85e2670d6aa8d568af535cf329280
dbs <dbs@9efc2488-bf62-4759-914b-345cdb29e865>
2008-02-16 18:12:57 +0000

    x.y.z version numbering seems to make setuptools happier... okay
    git-svn-id: svn://svn.open-ils.org/OpenSRF/trunk@1249 9efc2488-bf62-4759-914b-345cdb29e865

--- src/python/setup.py
+++ src/python/setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup
 
 setup(name='OpenSRF',
-    version='1.0',
+    version='1.0.0',
     install_requires=[
         'dnspython', # required by pyxmpp
-        'python_memcached',
+    	'python-memcached', # older setuptools fails to resolve this
         'pyxmpp>=1.0.0',
         'simplejson>=1.7.1'
     ],
     dependency_links = [
-        "http://pyxmpp.jajcus.net/downloads/"
+        "http://pyxmpp.jajcus.net/downloads/",
     ],
     description='OpenSRF Python Modules',
     author='Bill Erickson',
