1f9e0d35d6f74361107e61940ba427d8cbbb1129
dbs <dbs@9efc2488-bf62-4759-914b-345cdb29e865>
2008-04-16 01:37:05 +0000

    What was OpenSRF.js, is now DojoSRF.js - and there was much rejoicing from the Mac-heads
    git-svn-id: svn://svn.open-ils.org/OpenSRF/trunk@1312 9efc2488-bf62-4759-914b-345cdb29e865

---
+++ src/javascript/DojoSRF.js
@@ -0,0 +1,17 @@
+if(!dojo._hasResource['DojoSRF']){
+
+	dojo._hasResource['DojoSRF'] = true;
+	dojo.provide('DojoSRF');
+
+	// Note: this file was renamed from OpenSRF.js to DojoSRF.js,
+	// but still provides resources with the OpenSRF namespace
+	dojo.require('opensrf.opensrf', true);
+	dojo.require('opensrf.opensrf_xhr', true);
+
+	OpenSRF.session_cache = {};
+	OpenSRF.CachedClientSession = function ( app ) {
+		if (this.session_cache[app]) return this.session_cache[app];
+		this.session_cache[app] = new OpenSRF.ClientSession ( app );
+		return this.session_cache[app];
+	}
+}
