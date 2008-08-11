972593b2846e8ac190b76ac6ca7c5cb4cae6eace
sboyette <sboyette@9efc2488-bf62-4759-914b-345cdb29e865>
2008-08-11 19:32:50 +0000

    autogen.sh no longer blithely carries on and tells you to run ./configure even if the entire automake/autotools chain is missing. there's probably more elegant ways to do it, but this works.
    git-svn-id: svn://svn.open-ils.org/OpenSRF/branches/sboyette@1410 9efc2488-bf62-4759-914b-345cdb29e865

--- autogen.sh
+++ autogen.sh
@@ -15,6 +15,22 @@ ${AUTOMAKE} --add-missing
 
 ${AUTOCONF}
 
+SILENT=`which libtoolize aclocal autoheader automake autoconf`
+case "$?" in
+    0 )
+        echo All build tools found.
+        ;;
+    1)
+        echo
+        echo "--------------------------------------------------------------"
+        echo "          >>> Some build tools are missing! <<<"
+        echo Please make sure your system has the GNU autoconf and automake
+        echo toolchains installed.
+        echo "--------------------------------------------------------------"
+        exit
+        ;;
+esac
+
 echo 
 echo "---------------------------------------------"
 echo "autogen finished running, now run ./configure"
