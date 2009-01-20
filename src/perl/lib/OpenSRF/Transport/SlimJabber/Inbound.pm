df6140d234629ca6ae338de1fd099a8fa3d3302e
erickson <erickson@9efc2488-bf62-4759-914b-345cdb29e865>
2009-01-20 03:03:34 +0000

    repaired config data structure ref check for registering routers
    git-svn-id: svn://svn.open-ils.org/OpenSRF/trunk@1633 9efc2488-bf62-4759-914b-345cdb29e865

--- src/perl/lib/OpenSRF/Transport/SlimJabber/Inbound.pm
+++ src/perl/lib/OpenSRF/Transport/SlimJabber/Inbound.pm
@@ -98,7 +98,7 @@ sub listen {
         for my $router (@$routers) {
             if(ref $router) {
                 if( !$router->{services} || 
-                        ( ref($router->{services}) eq 'ARRAY' and 
+                        ( ref($router->{services}) eq 'HASH' and 
                             grep { $_ eq $self->{app} } @{$router->{services}->{service}} )  ||
                         $router->{services}->{service} eq $self->{app}) {
 
