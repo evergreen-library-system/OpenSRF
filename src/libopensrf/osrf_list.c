f90bf88f44fca544675bd1917a82bd0fcdc883f4
scottmk <scottmk@9efc2488-bf62-4759-914b-345cdb29e865>
2009-09-08 14:29:45 +0000

    Tweaking comments for doxygen:
    Moved the overview from the implementation file to the header.
    
    Deleted some comments from the header so that they wouldn't
    override more complete comments from the implementation file.
    
    
    git-svn-id: svn://svn.open-ils.org/OpenSRF/trunk@1770 9efc2488-bf62-4759-914b-345cdb29e865

--- src/libopensrf/osrf_list.c
+++ src/libopensrf/osrf_list.c
@@ -1,26 +1,6 @@
 /**
 	@file osrf_list.c	
 	@brief Implementation of osrfList, sort of like a vector class.
-	
-	An osrfList manages an array of void pointers, allocating additional memory as
-	needed when the array needs to grow.  Optionally, it may call a designated callback
-	function as a destructor when one of the void pointers is removed, or when the
-	array itself is destroyed.
-
-	The callback function must be of type void and accept a void pointer as its parameter.
-	There is no specific function for installing or uninstalling such a callback; just assign
-	directly to the freeItem member of the osrfList structure.
-
-	Unlike a typical vector class in, or example, C++, an osrfList does NOT shift items
-	around to fill in the gap when you remove an item.  Items stay put.
-
-	The use of void pointers involves the usual risk of type errors, so be careful.
-
-	NULL pointers are a special case.  On the one hand an osrfList will happily store
-	a NULL if you ask it to.  On the other hand it may overwrite a NULL pointer previously
-	stored, treating it as disposable.  Conclusion: you can store NULLs in an osrfList, but
-	not safely, unless you are familiar with the internal details of the implementation and
-	work around them accordingly.
 */
 
 #include <opensrf/osrf_list.h>
@@ -322,6 +302,9 @@ void* osrfListPop( osrfList* list ) {
 	@brief Create and initialize an osrfListIterator for a given osrfList.
 	@param list A pointer to an osrfList.
 	@return A pointer to the newly constructed osrfListIterator.
+
+	The calling code is responsible for freeing the osrfListIterator by calling
+		osrfListIteratorFree().
 */
 osrfListIterator* osrfNewListIterator( const osrfList* list ) {
 	if(!list) return NULL;
