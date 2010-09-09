694dc24b35b9ae1c5d68ec1a96d59c9f2fd2082c
dbs <dbs@9efc2488-bf62-4759-914b-345cdb29e865>
2010-09-09 17:09:42 +0000

    Bump trunk version number for OpenSRF up to 1.8.0; better than 1.4.0.
    perlmodstyle says "A correct CPAN version number is a floating point
    number with at least 2 digits after the decimal." so we're not in
    compliance in any case. Oh well.
    
    
    git-svn-id: svn://svn.open-ils.org/OpenSRF/trunk@2023 9efc2488-bf62-4759-914b-345cdb29e865

--- src/perl/lib/OpenSRF.pm
+++ src/perl/lib/OpenSRF.pm
@@ -14,11 +14,11 @@ OpenSRF - Top level class for OpenSRF perl modules.
 
 =head1 VERSION
 
-Version 1.4.0
+Version 1.8.0
 
 =cut
 
-our $VERSION = 1.4.0;
+our $VERSION = "1.8.0";
 
 =head1 METHODS
 
