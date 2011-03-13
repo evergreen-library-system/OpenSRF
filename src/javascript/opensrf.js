4f2ea4d15e4e323cb0198d6599e55ef4fcd8adcf
dbs <dbs@9efc2488-bf62-4759-914b-345cdb29e865>
2011-03-13 06:04:51 +0000

    Run OpenSRF JavaScript through jslint for a light cleanup
    Mostly just semicolons, and shifting a bit of code around to reduce
    usage of functions/classes before they've been defined. Staying away
    from the === / !== operators for null/undefined/0/''/true/false for
    now.
    
    
    git-svn-id: svn://svn.open-ils.org/OpenSRF/branches/rel_2_0@2210 9efc2488-bf62-4759-914b-345cdb29e865

--- src/javascript/opensrf.js
+++ src/javascript/opensrf.js
@@ -46,6 +46,146 @@ var OSRF_STATUS_INTERNALSERVERERROR = 500;
 var OSRF_STATUS_NOTIMPLEMENTED = 501;
 var OSRF_STATUS_VERSIONNOTSUPPORTED = 505;
 
+/* The following classes map directly to network-serializable opensrf objects */
+
+function osrfMessage(hash) {
+    this.hash = hash;
+    if(!this.hash.locale)
+        this.hash.locale = OpenSRF.locale || 'en-US';
+    this._encodehash = true;
+}
+osrfMessage.prototype.threadTrace = function(d) { 
+    if(arguments.length == 1) 
+        this.hash.threadTrace = d; 
+    return this.hash.threadTrace; 
+};
+osrfMessage.prototype.type = function(d) { 
+    if(arguments.length == 1) 
+        this.hash.type = d; 
+    return this.hash.type; 
+};
+osrfMessage.prototype.payload = function(d) { 
+    if(arguments.length == 1) 
+        this.hash.payload = d; 
+    return this.hash.payload; 
+};
+osrfMessage.prototype.locale = function(d) { 
+    if(arguments.length == 1) 
+        this.hash.locale = d; 
+    return this.hash.locale; 
+};
+osrfMessage.prototype.serialize = function() {
+    return {
+        "__c":"osrfMessage",
+        "__p": {
+            'threadTrace' : this.hash.threadTrace,
+            'type' : this.hash.type,
+            'payload' : (this.hash.payload) ? this.hash.payload.serialize() : 'null',
+            'locale' : this.hash.locale
+        }
+    };
+};
+
+function osrfMethod(hash) {
+    this.hash = hash;
+    this._encodehash = true;
+}
+osrfMethod.prototype.method = function() {
+    if(arguments.length == 1) 
+        this.hash.method = d; 
+    return this.hash.method; 
+};
+osrfMethod.prototype.params = function() {
+    if(arguments.length == 1) 
+        this.hash.params = d; 
+    return this.hash.params; 
+};
+osrfMethod.prototype.serialize = function() {
+    return {
+        "__c":"osrfMethod",
+        "__p": {
+            'method' : this.hash.method,
+            'params' : this.hash.params
+        }
+    };
+};
+
+function osrfMethodException(hash) {
+    this.hash = hash;
+    this._encodehash = true;
+}
+osrfMethodException.prototype.status = function() {
+    if(arguments.length == 1) 
+        this.hash.status = d; 
+    return this.hash.status; 
+};
+osrfMethodException.prototype.statusCode = function() {
+    if(arguments.length == 1) 
+        this.hash.statusCode = d; 
+    return this.hash.statusCode; 
+};
+function osrfConnectStatus(hash) { 
+    this.hash = hash;
+    this._encodehash = true;
+}
+osrfConnectStatus.prototype.status = function() {
+    if(arguments.length == 1) 
+        this.hash.status = d; 
+    return this.hash.status; 
+};
+osrfConnectStatus.prototype.statusCode = function() {
+    if(arguments.length == 1) 
+        this.hash.statusCode = d; 
+    return this.hash.statusCode; 
+};
+function osrfResult(hash) {
+    this.hash = hash;
+    this._encodehash = true;
+}
+osrfResult.prototype.status = function() {
+    if(arguments.length == 1) 
+        this.hash.status = d; 
+    return this.hash.status; 
+};
+osrfResult.prototype.statusCode = function() {
+    if(arguments.length == 1) 
+        this.hash.statusCode = d; 
+    return this.hash.statusCode; 
+};
+osrfResult.prototype.content = function() {
+    if(arguments.length == 1) 
+        this.hash.content = d; 
+    return this.hash.content; 
+};
+function osrfServerError(hash) { 
+    this.hash = hash;
+    this._encodehash = true;
+}
+osrfServerError.prototype.status = function() {
+    if(arguments.length == 1) 
+        this.hash.status = d; 
+    return this.hash.status; 
+};
+osrfServerError.prototype.statusCode = function() {
+    if(arguments.length == 1) 
+        this.hash.statusCode = d; 
+    return this.hash.statusCode; 
+};
+function osrfContinueStatus(hash) { 
+    this.hash = hash;
+    this._encodehash = true;
+}
+osrfContinueStatus.prototype.status = function() {
+    if(arguments.length == 1) 
+        this.hash.status = d; 
+    return this.hash.status; 
+};
+osrfContinueStatus.prototype.statusCode = function() {
+    if(arguments.length == 1) 
+        this.hash.statusCode = d; 
+    return this.hash.statusCode; 
+};
+
 OpenSRF = {};
 OpenSRF.locale = null;
 
@@ -56,23 +196,23 @@ OpenSRF.set_subclass = function(cls, pcls) {
     str += cls+'.baseClass = '+pcls+'.prototype.constructor;';
     str += cls+'.prototype["super"] = '+pcls+'.prototype;';
     eval(str);
-}
+};
 
 
 /* general session superclass */
 OpenSRF.Session = function() {
     this.remote_id = null;
     this.state = OSRF_APP_SESSION_DISCONNECTED;
-}
+};
 
 OpenSRF.Session.transport = OSRF_TRANSPORT_TYPE_XHR; /* default to XHR */
 OpenSRF.Session.cache = {};
 OpenSRF.Session.find_session = function(thread_trace) {
     return OpenSRF.Session.cache[thread_trace];
-}
+};
 OpenSRF.Session.prototype.cleanup = function() {
     delete OpenSRF.Session.cache[this.thread];
-}
+};
 
 OpenSRF.Session.prototype.send = function(osrf_msg, args) {
     args = (args) ? args : {};
@@ -82,23 +222,23 @@ OpenSRF.Session.prototype.send = function(osrf_msg, args) {
         case OSRF_TRANSPORT_TYPE_XMPP:
             return this.send_xmpp(osrf_msg, args);
     }
-}
+};
 
 OpenSRF.Session.prototype.send_xhr = function(osrf_msg, args) {
     args.thread = this.thread;
     args.rcpt = this.remote_id;
     args.rcpt_service = this.service;
     new OpenSRF.XHRequest(osrf_msg, args).send();
-}
+};
 
 OpenSRF.Session.prototype.send_xmpp = function(osrf_msg, args) {
     alert('xmpp transport not yet implemented');
-}
+};
 
 
 /* client sessions make requests */
 OpenSRF.ClientSession = function(service) {
-    this.service = service
+    this.service = service;
     this.remote_id = null;
     this.locale = OpenSRF.locale || 'en-US';
     this.last_id = 0;
@@ -106,7 +246,7 @@ OpenSRF.ClientSession = function(service) {
     this.requests = [];
     this.onconnect = null;
     OpenSRF.Session.cache[this.thread] = this;
-}
+};
 OpenSRF.set_subclass('OpenSRF.ClientSession', 'OpenSRF.Session');
 
 
@@ -131,7 +271,7 @@ OpenSRF.ClientSession.prototype.connect = function(args) {
     if(this.onconnect || this.state == OSRF_APP_SESSION_CONNECTED)
         return true;
     return false;
-}
+};
 
 OpenSRF.ClientSession.prototype.disconnect = function(args) {
     this.send(
@@ -141,7 +281,7 @@ OpenSRF.ClientSession.prototype.disconnect = function(args) {
         })
     );
     this.remote_id = null;
-}
+};
 
 
 OpenSRF.ClientSession.prototype.request = function(args) {
@@ -166,7 +306,7 @@ OpenSRF.ClientSession.prototype.request = function(args) {
     var req = new OpenSRF.Request(this, this.last_id++, args);
     this.requests.push(req);
     return req;
-}
+};
 
 OpenSRF.ClientSession.prototype.find_request = function(reqid) {
     for(var i = 0; i < this.requests.length; i++) {
@@ -175,7 +315,7 @@ OpenSRF.ClientSession.prototype.find_request = function(reqid) {
             return req;
     }
     return null;
-}
+};
 
 OpenSRF.Request = function(session, reqid, args) {
     this.session = session;
@@ -193,7 +333,7 @@ OpenSRF.Request = function(session, reqid, args) {
     this.timeout = args.timeout;
     this.response_queue = [];
     this.complete = false;
-}
+};
 
 OpenSRF.Request.prototype.peek_last = function(timeout) {
     if(this.response_queue.length > 0) {
@@ -202,19 +342,19 @@ OpenSRF.Request.prototype.peek_last = function(timeout) {
         return x;
     }
     return null;
-}
+};
 
 OpenSRF.Request.prototype.peek = function(timeout) {
     if(this.response_queue.length > 0)
         return this.response_queue[0];
     return null;
-}
+};
 
 OpenSRF.Request.prototype.recv = function(timeout) {
     if(this.response_queue.length > 0)
         return this.response_queue.shift();
     return null;
-}
+};
 
 OpenSRF.Request.prototype.send = function() {
     method = new osrfMethod({'method':this.method, 'params':this.params});
@@ -233,17 +373,17 @@ OpenSRF.Request.prototype.send = function() {
         'onmethoderror' : this.onmethoderror,
         'ontransporterror' : this.ontransporterror
     });
-}
+};
 
 OpenSRF.NetMessage = function(to, from, thread, body) {
     this.to = to;
     this.from = from;
     this.thread = thread;
     this.body = body;
-}
+};
 
 OpenSRF.Stack = function() {
-}
+};
 
 // global inbound message queue
 OpenSRF.Stack.queue = [];
@@ -263,7 +403,7 @@ OpenSRF.Stack.push = function(net_msg, callbacks) {
         var data = OpenSRF.Stack.queue.shift();
         OpenSRF.Stack.handle_message(data.ses, data.msg, data.callbacks);
     }
-}
+};
 
 OpenSRF.Stack.handle_message = function(ses, osrf_msg, callbacks) {
     
@@ -311,145 +451,6 @@ OpenSRF.Stack.handle_message = function(ses, osrf_msg, callbacks) {
                 return callbacks.onresponse(req);
         }
     }
-}
+};
 
-/* The following classes map directly to network-serializable opensrf objects */
-
-function osrfMessage(hash) {
-    this.hash = hash;
-    if(!this.hash.locale)
-        this.hash.locale = OpenSRF.locale || 'en-US';
-    this._encodehash = true;
-}
-osrfMessage.prototype.threadTrace = function(d) { 
-    if(arguments.length == 1) 
-        this.hash.threadTrace = d; 
-    return this.hash.threadTrace; 
-}
-osrfMessage.prototype.type = function(d) { 
-    if(arguments.length == 1) 
-        this.hash.type = d; 
-    return this.hash.type; 
-}
-osrfMessage.prototype.payload = function(d) { 
-    if(arguments.length == 1) 
-        this.hash.payload = d; 
-    return this.hash.payload; 
-}
-osrfMessage.prototype.locale = function(d) { 
-    if(arguments.length == 1) 
-        this.hash.locale = d; 
-    return this.hash.locale; 
-}
-osrfMessage.prototype.serialize = function() {
-    return {
-        "__c":"osrfMessage",
-        "__p": {
-            'threadTrace' : this.hash.threadTrace,
-            'type' : this.hash.type,
-            'payload' : (this.hash.payload) ? this.hash.payload.serialize() : 'null',
-            'locale' : this.hash.locale
-        }
-    };
-}
-
-function osrfMethod(hash) {
-    this.hash = hash;
-    this._encodehash = true;
-} 
-osrfMethod.prototype.method = function() {
-    if(arguments.length == 1) 
-        this.hash.method = d; 
-    return this.hash.method; 
-}
-osrfMethod.prototype.params = function() {
-    if(arguments.length == 1) 
-        this.hash.params = d; 
-    return this.hash.params; 
-}
-osrfMethod.prototype.serialize = function() {
-    return {
-        "__c":"osrfMethod",
-        "__p": {
-            'method' : this.hash.method,
-            'params' : this.hash.params
-        }
-    };
-}
-
-function osrfMethodException(hash) {
-    this.hash = hash;
-    this._encodehash = true;
-}
-osrfMethodException.prototype.status = function() {
-    if(arguments.length == 1) 
-        this.hash.status = d; 
-    return this.hash.status; 
-}
-osrfMethodException.prototype.statusCode = function() {
-    if(arguments.length == 1) 
-        this.hash.statusCode = d; 
-    return this.hash.statusCode; 
-}
-function osrfConnectStatus(hash) { 
-    this.hash = hash;
-    this._encodehash = true;
-}
-osrfConnectStatus.prototype.status = function() {
-    if(arguments.length == 1) 
-        this.hash.status = d; 
-    return this.hash.status; 
-}
-osrfConnectStatus.prototype.statusCode = function() {
-    if(arguments.length == 1) 
-        this.hash.statusCode = d; 
-    return this.hash.statusCode; 
-}
-function osrfResult(hash) {
-    this.hash = hash;
-    this._encodehash = true;
-}
-osrfResult.prototype.status = function() {
-    if(arguments.length == 1) 
-        this.hash.status = d; 
-    return this.hash.status; 
-}
-osrfResult.prototype.statusCode = function() {
-    if(arguments.length == 1) 
-        this.hash.statusCode = d; 
-    return this.hash.statusCode; 
-}
-osrfResult.prototype.content = function() {
-    if(arguments.length == 1) 
-        this.hash.content = d; 
-    return this.hash.content; 
-}
-function osrfServerError(hash) { 
-    this.hash = hash;
-    this._encodehash = true;
-}
-osrfServerError.prototype.status = function() {
-    if(arguments.length == 1) 
-        this.hash.status = d; 
-    return this.hash.status; 
-}
-osrfServerError.prototype.statusCode = function() {
-    if(arguments.length == 1) 
-        this.hash.statusCode = d; 
-    return this.hash.statusCode; 
-}
-function osrfContinueStatus(hash) { 
-    this.hash = hash;
-    this._encodehash = true;
-}
-osrfContinueStatus.prototype.status = function() {
-    if(arguments.length == 1) 
-        this.hash.status = d; 
-    return this.hash.status; 
-}
-osrfContinueStatus.prototype.statusCode = function() {
-    if(arguments.length == 1) 
-        this.hash.statusCode = d; 
-    return this.hash.statusCode; 
-}
 
