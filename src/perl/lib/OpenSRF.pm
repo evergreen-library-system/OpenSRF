394c97ef9f07c1baaab43a6cca3f60b287b87f19
Jason Stephenson <jason@sigio.com>
2024-09-30 20:25:26 +0000

    Bump version numbers for 3.2.6
    Signed-off-by: Jason Stephenson <jason@sigio.com>

--- src/perl/lib/OpenSRF.pm
+++ src/perl/lib/OpenSRF.pm
@@ -12,11 +12,11 @@ OpenSRF - Top level class for OpenSRF perl modules.
 
 =head1 VERSION
 
-Version 3.2.5
+Version 3.2.6
 
 =cut
 
-our $VERSION = "3.002005";
+our $VERSION = "3.002006";
 
 =head1 METHODS
 
--- version.m4
+++ version.m4
@@ -1,1 +1,1 @@
-m4_define([VERSION_NUMBER],[3.2.5]) 
+m4_define([VERSION_NUMBER],[3.2.6]) 
